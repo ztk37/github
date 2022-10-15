b1668f897922121baeebdf4e18cc976fdad45f39
ztk37 <ztk37.contact@gmail.com>
2022-10-15 10:55:45 +0200

    update: manage this repo with terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
 terraform {
-  
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+resource "github_repository" "github" {
+  name        = "github"
+  description = ":construction: :globe_with_meridians: Explore GitHub features"
+  topics      = ["terraform", "github", "automation"]
+}
+
+variable "github_token" {
+  type        = string
+  description = "required: GitHub Personal Access Token"
 }
