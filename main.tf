c91eaf7882a9b3f3aa68eb0edfcfffdda928d0f1
ztk37 <ztk37.contact@gmail.com>
2022-10-15 11:31:30 +0200

    update: extend repository resource

--- main.tf
+++ main.tf
@@ -15,6 +15,11 @@ resource "github_repository" "github" {
   name        = "github"
   description = ":construction: :globe_with_meridians: Explore GitHub features"
   topics      = ["terraform", "github", "automation"]
+
+  # note: Currently, GitHub discussions and the wiki can't be managed by the GitHub provider. Maybe in the future.
+  has_issues   = true
+  has_projects = true
+  has_wiki     = true
 }
 
 variable "github_token" {
